f2b59dd6ed559ffd660df0a21d2e78954fd1cb37
Andresluna93 <andresucsg93@gmail.com>
2025-06-24 14:39:49 -0800

    vercel json para backend

--- clientmania/src/api/auth.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-const API = 'http://localhost:3000/api'
-
-export const registerRequest = user => axios.post(`${API}/estudiante`,user)
-export const registerEmbajadorRequest = user => axios.post(`${API}/embajador`,user)
