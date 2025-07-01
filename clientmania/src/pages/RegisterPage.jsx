665f62f19f00afd803570f74e33539ffc5373a69
Andresluna93 <andresucsg93@gmail.com>
2025-07-01 18:52:35 -0500

    se actualizo una dependencia y el select de register page

--- clientmania/src/pages/RegisterPage.jsx
+++ clientmania/src/pages/RegisterPage.jsx
@@ -2,6 +2,7 @@ import {useForm} from 'react-hook-form'
 import {registerRequest} from '../api/auth'
 import {z} from 'zod'
 import {zodResolver} from '@hookform/resolvers/zod'
+import { ChevronDownIcon } from '@heroicons/react/16/solid'
 
 const validationSchema = z.object({
 	username: z.string().min(8,'numero de caracteres minimos 8'),
@@ -35,15 +36,28 @@ function RegisterPage(){
                 <input type="email" {...register("email",{require:true})} className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2' placeholder='Correo'></input>
                 {errors.email && <span>{errors.email.message}</span>}
                 <br />
-                <label htmlFor='colegio'>Colegio</label>
-			    <select {...register("colegio",{require:true})} className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'>
-				<option value='selecciona'>Selecciona..</option>
-				<option value='Logos'>Logos</option>
-				<option value='Americano'>Americano</option>
-				<option value='Copol'>Copol</option>
-			     </select>
-                 {errors.colegio && <span>{errors.colegio.message}</span>}
-                 <br />
+                 <div className="sm:col-span-3">
+              <label htmlFor="colegio" className="block text-sm/6 font-medium">
+                Colegio
+              </label>
+              <div className="mt-2 grid grid-cols-1">
+                <select
+                  {...register("colegio",{required:true})}
+                  className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-zinc-700 py-1.5 pr-8 pl-3 text-base text-white outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                >
+                  <option value='selecciona'>Selecciona</option>
+                  <option value='Logos'>Logos</option>
+				  <option value='Americano'>Americano</option>
+				  <option value='Copol'>Copol</option>
+                </select>
+                <ChevronDownIcon
+                  aria-hidden="true"
+                  className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                />
+              </div>
+                {errors.colegio && <span>{errors.colegio.message}</span>}
+                <br />
+            </div>
                  <button type='submit' className='rounded-md bg-zinc-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 my-2'>
                     Enviar
                  </button>
